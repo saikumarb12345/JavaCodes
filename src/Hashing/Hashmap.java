7f6804cee308f91f8e0dac10a412d7cb41b14493
SaiKumar <bsainaidu90@gmail.com>
2022-12-15 22:48:57 +0530

    15-12-2022 Hashing Intro commit

---
+++ src/Hashing/Hashmap.java
@@ -0,0 +1,40 @@
+package Hashing;
+import java.util.HashMap;
+public class Hashmap {
+    public static void main(String[] args) {
+        //Create
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        //Insert - O(1) --> Insert Key,Values
+        hm.put("Bharath",100);
+        hm.put("china",150);
+        hm.put("usa",50);
+        //hm.put("Bharath",130);
+
+        System.out.println(hm);
+
+//        Get - O(1) --> Get values
+//        int population = hm.get("Bharath");
+//        System.out.println(population); // Returns 100
+//        System.out.println(hm.get("Japan")); // Returns Null if there is no key in hashmap
+//
+//        ContainsKey - O(1) --> Check for key
+//        System.out.println(hm.containsKey("Bharath")); // True
+//        System.out.println(hm.containsKey("Indonesia")); // False
+
+        //Remove - O(1)
+//        System.out.println(hm.remove("china")); // Returns Value(150)
+//        System.out.println(hm.remove("Indonesia")); // Returns null if key doesn't exists
+//        System.out.println(hm);
+
+        // Size
+        //System.out.println("size of hashmap is " +hm.size()); // Returns size of the HashMap
+
+        // Is Empty
+//        System.out.println(hm.isEmpty()); // false
+//        hm.clear();                       // clears all the Hashmap
+//        System.out.println(hm.isEmpty()); // true
+
+
+    }
+}
---
+++ src/Hashing/HashmapIteration.java
@@ -0,0 +1,27 @@
+package Hashing;
+
+import java.util.HashMap;
+import java.util.Set;
+
+// Iteration on Hashmap
+public class HashmapIteration {
+    public static void main(String[] args) {
+        HashMap<String,Integer> hm = new HashMap<>();
+        hm.put("Bharath",100);
+        hm.put("china",150);
+        hm.put("usa",50);
+        hm.put("uk",30);
+        hm.put("Indonesia",10);
+        hm.put("Combodia",5);
+        System.out.println(hm);
+
+        //Iterate
+        Set<String> keys = hm.keySet();
+        System.out.println(keys);
+
+        for (String k : keys){
+            System.out.println("key="+k+", value="+hm.get(k));
+        }
+
+    }
+}
